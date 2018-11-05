bbde839aa5d6aa15ab3e158a9ce110ff5c0b3e06
ciric_bogdan <ciric.bogdan314@gmail.com>
2018-11-05 23:36:15 +0100

    Bomb, gravity and collision added

--- src/me/boom/Game.java
+++ src/me/boom/Game.java
@@ -4,8 +4,10 @@ import rafgfxlib.GameFrame;
 import rafgfxlib.Util;
 
 import java.awt.*;
+import java.awt.geom.Area;
 import java.awt.image.BufferedImage;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Game extends GameFrame {
 
@@ -13,6 +15,16 @@ public class Game extends GameFrame {
 
     private ArrayList<Tile> tiles;
     private Tile player;
+
+    private int bombCnt;
+    private Tile bomb;
+
+    private double angle = 0;
+    private double speedX = 0;
+    private double speedY = 0;
+    private double radius = 10.0;
+    private double gravity = -0.1;
+
 
     private int windowWidth;
     private int windowHeight;
@@ -35,20 +47,23 @@ public class Game extends GameFrame {
         int rows = bitMap.length;
         int columns = bitMap[0].length;
 
-        for(int i = 0; i < bitMap.length; i++) {
-            for(int j = 0; j < bitMap[i].length; j++) {
-                int width = this.windowWidth/columns;
-                int height = this.windowHeight/rows;
+        for (int i = 0; i < bitMap.length; i++) {
+            for (int j = 0; j < bitMap[i].length; j++) {
+                int width = this.windowWidth / columns;
+                int height = this.windowHeight / rows;
 
 
-                if(bitMap[i][j].equals("#")) {
-                    Tile tile = new Tile("Tiles/"+"player"+".png",
-                            j*width, i*height, width, height);
+                if (bitMap[i][j].equals("#")) {
+                    Tile tile = new Tile("Tiles/" + "player" + ".png",
+                            j * width, i * height, width, height, "p");
                     player = tile;
-                }
-                else {
-                    Tile tile = new Tile("Tiles/"+bitMap[i][j]+".png",
-                            j*width, i*height, width, height);
+                } else {
+                    Tile tile = new Tile("Tiles/" + bitMap[i][j] + ".png",
+                            j * width, i * height, width, height, "");
+                    if (bitMap[i][j].equals("0"))
+                        tile.setType("0");
+                    else
+                        tile.setType("g");
                     tiles.add(tile);
                 }
             }
@@ -59,18 +74,21 @@ public class Game extends GameFrame {
     public void handleWindowInit() {
         loadTiles();
 
-        setUpdateRate(60);
+        setUpdateRate(90);
         startThread();
     }
 
     @Override
     public void render(Graphics2D g, int i, int i1) {
-        g.drawImage(background,0, 0, null );
+        g.drawImage(background, 0, 0, null);
 
         for (Tile tile : this.tiles) {
             g.drawImage(tile.getImage(), tile.getX(), tile.getY(), tile.getWidth(), tile.getHeight(), null);
         }
         g.drawImage(player.getImage(), player.getX(), player.getY(), player.getWidth(), player.getHeight(), null);
+        if (bomb != null) {
+            g.drawImage(bomb.getImage(), bomb.getX(), bomb.getY(), bomb.getWidth(), bomb.getHeight(), null);
+        }
 
 //        g.setColor(Color.yellow);
 //        g.drawRect(selX * TILE_W / 4, selY * TILE_H / 4, TILE_W / 4, TILE_H / 4);
@@ -78,6 +96,26 @@ public class Game extends GameFrame {
 
     @Override
     public void update() {
+
+        // na klik se pojavljuje bomba
+
+        if (isMouseButtonDown(GFMouseButton.Left)) {
+            if (bomb == null) {
+                bombCnt = 1;
+                bomb = new Tile("Tiles/Bomb.png", getMouseX(), getMouseY(), 30, 30, "b");
+            }
+            bombCnt++;
+            if (bombCnt % 15 == 0) {
+                bomb.setHeight(bomb.getHeight() + 2);
+                bomb.setWidth(bomb.getWidth() + 2);
+            }
+        } else if (bomb != null) {
+            handleBoom();
+            bomb = null;
+        }
+
+        handleMovement();
+
 
     }
 
@@ -109,5 +147,174 @@ public class Game extends GameFrame {
     @Override
     public void handleKeyUp(int i) {
 
+    }
+
+    public void handleBoom() {
+
+        setAngle();
+        setSpeed();
+
+    }
+
+    public void setAngle() {
+
+        angle = Math.toDegrees(Math.atan2(player.getY() + (player.getHeight() / 2) - bomb.getY(), player.getX() + (player.getWidth() / 2) - bomb.getX()));
+
+        if (angle < 0)
+            angle += 360;
+
+
+        angle /= 180;
+        angle *= Math.PI;
+
+        if (angle <= Math.PI) {
+            gravity = -0.1;
+        } else {
+            gravity = 0.1;
+        }
+
+    }
+
+    public void setSpeed() {
+
+        speedX = 7;
+        speedY = 7;
+
+
+    }
+
+    public void handleMovement() {
+
+        speedY -= gravity;
+
+        double dX = (float) (Math.cos(angle) * speedX);
+        double dY = (float) (Math.sin(angle) * speedY);
+
+        if (!handleColision(dX, dY)) {
+
+
+            player.setX((int) (player.getX() + dX));
+            player.setY((int) (player.getY() + dY));
+        }
+        //
+        //
+// }
+
+    }
+
+    private boolean handleColision(double dX, double dY) {
+
+        for (Tile t : tiles) {
+            if (t.getType().equals("g")) {
+//                (x1 + w1) - x2 >= 0 and (x2 + w2) - x1 >= 0
+
+
+                Area temp = new Area(t.getShape());
+                if (temp.intersects((int) (player.getX() + dX), (int) (player.getY() + dY), player.getWidth(), player.getHeight())) {
+                    Scanner sc = new Scanner(System.in);
+//                    sc.next();
+                    System.out.println((player.getY() + " " + player.getX() + " " + t.getX() + " " + t.getY() + " " + t.getHeight() + " " + t.getWidth()));
+//
+//
+//
+//                    if (!(player.getX() > t.getX())) {
+//                        angle = Math.PI - angle;
+//                        if(dX > 0) {
+//                            player.setX(t.getX() - player.getWidth() - 2);
+//                            System.out.println("evo me preko dx > 0");
+//
+//                        } else {
+//                            System.out.println("evo me preko dx < 0");
+//
+//                            player.setX(t.getX() + t.getWidth() + 2);
+//                        }
+//
+//                    } else {
+//                        angle = 2 * Math.PI - angle;
+//                        if(dY > 0) {
+//                            System.out.println("evo me preko dy > 0");
+//
+//                            player.setY(t.getY() - player.getHeight() - 2);
+//                            speedY = 0;
+//                            speedX = 0;
+//                        } else {
+//                            System.out.println("evo me preko dy < 0");
+//
+//                            player.setY(t.getY() + t.getHeight() + 2);
+//                        }
+//                        gravity *= -1;
+//                    }
+
+                    if (dX < 0 && dY < 0) {
+                        if (player.getY() <= t.getY() + t.getHeight()) {
+                            System.out.println("evo me preko dy > 0");
+                            player.setX(t.getX() + t.getWidth() + 2);
+                            angle = Math.PI - angle;
+                        } else {
+                            System.out.println("evo me preko dy < 0");
+                            player.setY(t.getY() + t.getHeight() + 2);
+                            angle = 2 * Math.PI - angle;
+                            gravity *= -1;
+                        }
+                    }
+
+                    if (dX > 0 && dY < 0) {
+                        if (player.getY() <= t.getY() + t.getHeight()) {
+                            System.out.println("evo me preko dy > 0");
+                            player.setX(t.getX() - t.getWidth());
+                            angle = Math.PI - angle;
+
+                        } else {
+                            System.out.println("evo me preko dy < 0");
+                            player.setY(t.getY() + t.getHeight() + 2);
+                            angle = 2 * Math.PI - angle;
+                            gravity *= -1;
+                        }
+                    }
+
+                    if (dX < 0 && dY > 0) {
+
+                        if (player.getY() + player.getHeight() <= t.getY()) {
+                            speedX = 0;
+                            speedY = 0;
+                            System.out.println("evo me preko dy > 0");
+                            player.setY(t.getY() - player.getHeight() - 2);
+                            angle = Math.PI - angle;
+                        } else {
+                            System.out.println("evo me preko dy < 0");
+//                            player.setX(t.getX() + t.getWidth() + 2);
+                            angle = 2 * Math.PI - angle;
+                            gravity *= -1;
+
+                        }
+                    }
+
+                    if (dX > 0 && dY > 0) {
+                        if (player.getY() + player.getHeight() <= t.getY()) {
+                            speedX = 0;
+                            speedY = 0;
+                            System.out.println("evo me preko dy > 0");
+                            player.setY(t.getY() - player.getHeight() - 2);
+                            angle = Math.PI - angle;
+                        } else {
+
+                            System.out.println("evo me preko dy < 0");
+                            player.setX(t.getX() - player.getWidth() - 4);
+                            angle = 2 * Math.PI - angle;
+                            gravity *= -1;
+
+                        }
+                    }
+
+                    if(speedX > 0)
+                        speedX -= 0.3;
+
+                    speedY += gravity;
+
+                    return true;
+                }
+            }
+        }
+        return false;
     }
 }
--- src/me/boom/Tile.java
+++ src/me/boom/Tile.java
@@ -1,9 +1,11 @@
 package me.boom;
 
 import rafgfxlib.Util;
+
+import java.awt.*;
 import java.awt.image.BufferedImage;
 
-class Tile
+public class Tile
 {
 
     private BufferedImage image = null;
@@ -13,15 +15,22 @@ class Tile
     private int width;
     private int height;
 
+    private Shape shape;
 
-    public Tile(String fileName, int x, int y, int width, int height)
+    private String type;
+
+
+    public Tile(String fileName, int x, int y, int width, int height, String type)
     {
         this.image = Util.loadImage(fileName);
+
+        this.shape = new Rectangle(x, y, width, height);
 
         this.x = x;
         this.y = y;
         this.width = width;
         this.height = height;
+        this.type = type;
 
         if(this.image == null)
         {
@@ -47,5 +56,41 @@ class Tile
 
     public int getHeight() {
         return height;
+    }
+
+    public void setImage(BufferedImage image) {
+        this.image = image;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public void setWidth(int width) {
+        this.width = width;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public Shape getShape() {
+        return shape;
+    }
+
+    public void setShape(Shape shape) {
+        this.shape = shape;
     }
 }
